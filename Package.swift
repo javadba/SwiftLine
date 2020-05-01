4365977a5116ce65f2f9ea286a07747550ed1b47
javadba <javadba@gmail.com>
2020-04-30 22:13:20 -0700

    Renamed Swiftline SwiftLine

--- Package.swift
+++ Package.swift
@@ -2,21 +2,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swiftline",
+    name: "SwiftLine",
     platforms: [
         .macOS(.v10_13),
     ],
     products: [
-        .library(name: "Swiftline", targets: ["Swiftline"]),
+        .library(name: "SwiftLine", targets: ["SwiftLine"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.7"),
     ],
     targets: [
-        .target(name: "Swiftline", path: "Sources"),
+        .target(name: "SwiftLine", path: "Sources"),
         .testTarget(name: "SwiftlineTests",
-                    dependencies: [.target(name: "Swiftline"),
+                    dependencies: [.target(name: "SwiftLine"),
                                    .product(name: "Quick"),
                                    .product(name: "Nimble")],
                     path: "Tests"),
